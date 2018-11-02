f6fe697c64ed5d3b38815fac695caecaa609f337
Angel <angelmmed12@gmail.com>
2018-11-02 09:19:50 -0500

    fix rare error where server would 500 if there isn't a request referrer.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,19 @@
 class SessionsController < ApplicationController
   def as_admin
     session[:is_admin?] = true
-    redirect_to request.referrer
+    if request.referrer.nil?
+      redirect_to root_path
+    else
+      redirect_to request.referrer 
+    end
   end
 
   def as_user
     session[:is_admin?] = false
-    redirect_to request.referrer
+    if request.referrer.nil?
+      redirect_to root_path
+    else
+      redirect_to request.referrer 
+    end
   end
 end
